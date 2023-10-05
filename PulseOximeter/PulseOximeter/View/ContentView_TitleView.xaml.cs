ad950b6059c00499a24017fda6dae525be5cd5f7
David <15479248+davepruitt@users.noreply.github.com>
2023-10-05 15:57:49 -0600

    Getting the UI of the "standard view" organized

---
+++ PulseOximeter/PulseOximeter/View/ContentView_TitleView.xaml.cs
@@ -0,0 +1,9 @@
+namespace PulseOximeter.View;
+
+public partial class ContentView_TitleView : ContentView
+{
+	public ContentView_TitleView()
+	{
+		InitializeComponent();
+	}
+}
---
+++ PulseOximeter/PulseOximeter/ViewModel/MainPageViewModel.cs
@@ -0,0 +1,21 @@
+﻿using PulseOximeter.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PulseOximeter.ViewModel
+{
+    public class MainPageViewModel : NotifyPropertyChangedObject
+    {
+        #region Constructor
+
+        public MainPageViewModel()
+        {
+            //empty
+        }
+
+        #endregion
+    }
+}
